106d99e378b581da27377250ed594e74c58aa9e1
morpheu513 <chiragkashyap15@gmail.com>
2020-09-26 16:26:05 +0530

    Updated Code for task 1

--- assignment_2/task_1/mapper.py
+++ assignment_2/task_1/mapper.py
@@ -5,15 +5,9 @@ import sys
 
 curr_node = None
 
-fptr = open('v', 'a')
-
 for line in sys.stdin:
     ip = line.split()
     from_node = ip[0]
     to_node = ip[1]
-    if curr_node != from_node:
-        fptr.write("%s,\t1\n" % from_node)
-        curr_node = from_node
-    print(from_node,"\t",to_node)
-
-fptr.close()
+    if from_node != '#':
+        print(from_node,"\t",to_node)
--- assignment_2/task_1/reducer.py
+++ assignment_2/task_1/reducer.py
@@ -5,29 +5,26 @@ import sys
 
 curr_node = None
 
-fptr = open('adj_list', 'a')
+v_file_path = sys.argv[1]
+
+fptr_v_file = open(v_file_path, 'a')
 
 for line in sys.stdin:
     from_node, to_node = line.split("\t")
+    to_node = to_node.rstrip("\n")
     if curr_node != from_node:
         if curr_node == None:
             node_adj_list = list()
             node_adj_list.append(to_node)
             curr_node = from_node
         else:
-            fptr.write("%s\t[" % curr_node)
-            for item in range(0,len(node_adj_list)):
-                if item == len(node_adj_list)-1:
-                    fptr.write("%s, " % item)
-                else:
-                    fptr.write("%s]" % item)
+            print(curr_node,"  ",node_adj_list,"\n")
+            fptr_v_file.write("%s\t1\n" % curr_node)
             node_adj_list = list()
             node_adj_list.append(to_node)
             curr_node = from_node
-    else:
-        if curr_node == None:
-            pass
-        elif curr_node == from_node:
-            node_adj_list.append(to_node)
+    elif curr_node == from_node:
+        node_adj_list.append(to_node)
 
-fptr.close()
+if(curr_node == from_node):
+    print(curr_node,"\t",node_adj_list,"\n")
