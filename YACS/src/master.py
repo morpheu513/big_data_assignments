09b6dfde677a5aa4e9c11061f27a8d9056e72589
morpheu513 <chiragkashyap15@gmail.com>
2020-12-03 19:52:59 +0530

    Added comments to worker.py and changed directory structure

--- YACS/src/master.py
+++ YACS/src/master.py
@@ -9,34 +9,21 @@ from datetime import datetime
 
 
 def addToDict_and_queue(job_id,map_tasks,reduce_tasks):
-    #dictLock.acquire()
-    #print("#dictLock released \n")
-    #queueLock.acquire()
-    #print("Queuelock released \n") 
-   # print("Acquired Dict and queue lock")
+
     jobDict[job_id]=[[],[],0]
+    
     for i in map_tasks:
         jobDict[job_id][0].append(i['task_id'])
         mapQ.put((job_id,i['task_id'],i['duration']))
-
-    #print("MAP : ",mapQ,"\n")
 
     for j in reduce_tasks:
         jobDict[job_id][1].append(j['task_id'])
         jobDict[job_id][2]+=1
         redQ.put((job_id,j['task_id'],j['duration']))
 
-    #print("REDUCE : ",redQ,"\n")
-    #print(mapQ.queue[-1])
-    #print(redQ.queue[-1])
-    #print("Brahmilamila")
-    #queueLock.release()
-    #print("#queueLock released \n")
-    #dictLock.release()
-    #print("#dictLock released \n")
-   # print("Releasing Dict and queue lock")
 
 def listen_incoming_jobs(receive_jobs_addr):
+    
     jobs_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     jobs_sock.bind(receive_jobs_addr)
     jobs_sock.listen(5)
@@ -44,9 +31,9 @@ def listen_incoming_jobs(receive_jobs_addr):
     while True:
         print("Listening to jobs....")
         conn, client_address = jobs_sock.accept()
+        
         while True:
-            #finalAnswer.acquire()
-            #print("Acquired job lock")
+
             data = conn.recv(2048)
             if data:
                 data = data.decode('utf-8')
@@ -63,60 +50,46 @@ def listen_incoming_jobs(receive_jobs_addr):
                 reduce_tasks = data["reduce_tasks"]
 
                 addToDict_and_queue(job_id,map_tasks,reduce_tasks)
-
-                #for i in map_tasks:
-                    #pass
-                    #print(i)
-
-                #print(data["job_id"])
                 
             else:
-                #print("No more incoming jobs..")
                 break
-            #finalAnswer.release()
-            #print("Released job lock")
 
         conn.close()
 
+
 def remDict(job_id,task_id):
-    #dictLock.acquire()
-    print("acquired dict lock in rem dict")
 
     if 'M' in task_id:
         jobDict[job_id][0].remove(task_id)
+    
     else:
         jobDict[job_id][1].remove(task_id)
+        
         if jobDict[job_id][1]==[]:
-            #print("JOBS ARE ACTUALLY GETTING COMPLETED")
             print("Job with ID: ", job_id," COMPLETED\n")
 
-    #dictLock.release()
-    print("released dict lock")
 
 def updateSlots(worker_id):
-    #workLock.acquire()
-    print("Work lock acquired in update slots")
 
-    workers_array[int(worker_id)-1][1]+=1   #might have to change logic here
-
-    #workLock.release()
-    #print("Work lock released")
-
+    workers_array[int(worker_id)-1][1]+=1
 
 
 def listen_worker_updates(worker_updates_addr):
+    
     updates_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     updates_sock.bind(worker_updates_addr)
     updates_sock.listen(5)
 
     while True:
+        
         print("Listening to Updates from workers....")
         conn, worker_address = updates_sock.accept()
+        
         while True:
-            #finalAnswer.acquire()
-            #print("Acquired worker lock")
+            
             data = conn.recv(2048)
             if data:
+                
                 data=data.decode('utf-8')
                 worker_id,job_id,task_id=data.split(',')
                 print("WORKER ",worker_id,"CAME BACK")
@@ -124,17 +97,15 @@ def listen_worker_updates(worker_updates_addr):
                 updateSlots(worker_id)
 
             else:
-              #  print("All workers have finished executing..")
                 break
             
-            #finalAnswer.release()
-            #print("Released worker lock")
-
         conn.close()
+
 
 def sendTask(task,worker):
     
     isEnd="0"
+    
     if jobDict[task[0]][2]==0:
     	isEnd="1"
     	
@@ -144,119 +115,111 @@ def sendTask(task,worker):
                 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as toWorker:
         toWorker.connect(("localhost", worker[2]))
-        message=isEnd+','+str(task[0])+','+str(task[1])+','+str(task[2])+','+schedule_algo  #SENDING SCHEDULING ALGO EVERYTIME
-        toWorker.send(message.encode()) #send task to worker
+        message=isEnd+','+str(task[0])+','+str(task[1])+','+str(task[2])+','+schedule_algo 
+        toWorker.send(message.encode())
 
 
 def round_robin(item):
     done=False
+    
     while not done:
         rr_choice[0]=(rr_choice[0]+1)%len(workers_array)
 
-        #workLock.acquire()
-       # print("Work lock acquired")
-
         if workers_array[rr_choice[0]][1]!=0:
-            workers_array[rr_choice[0]][1]-=1
             
-            #workLock.release()
-           # print("Work lock released")
-
+            workers_array[rr_choice[0]][1]-=1
             sendTask(item,workers_array[rr_choice[0]])
             done=True
 
+
 def least_loaded(item):
+    
     done=False
+    
     while not done:
-        #workLock.acquire()
-       # print("Work lock acquired")
 
         maxWorker=0
 
         for i in range(1,len(workers_array)):
+            
             if workers_array[maxWorker][1]<workers_array[i][1]:
                 maxWorker=i
+        
         if workers_array[i]==0:
             time.sleep(1)
+        
         else:
             workers_array[maxWorker][1] -= 1
             sendTask(item,workers_array[maxWorker])
             done=True
-
-        #workLock.release()
-       # print("Work lock released")
-
-        
+       
 
 def random_sched(item):
+    
     done=False
+    
     while not done:
         choice=random.randrange(0,len(workers_array))
 
-        #workLock.acquire()
-        #print("Work lock acquired")
-
         if workers_array[choice][1]!=0:
-            workers_array[choice][1]-=1
             
-            #workLock.release()
-          #  print("Work lock released")
-
+            workers_array[choice][1]-=1
             sendTask(item,workers_array[choice])
             done=True
 
 
-
-
 def scheduleItem(item):
+    
     if schedule_algo=="RANDOM":
         random_sched(item)
+    
     elif schedule_algo=="RR":
         round_robin(item)
+    
     elif schedule_algo=="LL":
         least_loaded(item)
+    
     else:
         print("Unexpected scheduling algo, program will not work")
 
 
-
 def slots_available():
     for i in range(len(workers_array)):
+        
         if workers_array[i][1]!=0:  #CHeck here once
             return True
+    
     return False
 
 def scheduleTasks():
+    
     while True:
         
         if slots_available():
-            #dictLock.acquire() #CHANGED THIS LOCK (ADDED IT BACK)
+            
             flag=1
             item=-1
-            #queueLock.acquire()
-            #print("queue lock acquired")
+
             if not redQ.empty():
+                
                 amogha=redQ.queue[0]
-                if jobDict[amogha[0]][0] == []:#potential dict lock needed here,  need to send some sort of flag indication last reduce task for logging?
+                if jobDict[amogha[0]][0] == []:
+                    
                     flag=0
                     item=redQ.get()
                     jobDict[item[0]][2]-=1
-                    print("popped reduce queue",item,"\n")
+            
             if flag==1 and not mapQ.empty():
                 item=mapQ.get()
+            
             if item!=-1:
                 scheduleItem(item)
-            #dictLock.release()
-            #queueLock.release()
-            #time.sleep(2) 
+        
         else:
-            print("slots are filled go die") 
-        ##dictLock.release()
-            #print("queue lock released")
-        
-        
-        
+            #print("slots are filled go die")
+            pass 
 
+        
 if __name__ == '__main__':
     
 
@@ -266,7 +229,7 @@ if __name__ == '__main__':
     config_file = open(path_to_config)
     config = json.load(config_file)
     
-    fileName=schedule_algo+'/master'+schedule_algo+'.csv'
+    fileName='../data/'+schedule_algo+'/master'+schedule_algo+'.csv'
     logging.basicConfig(level=logging.INFO,filename=fileName, filemode='w', format='%(message)s')
     logging.info('Type,ID,Time')
     workers_array = list()
@@ -287,10 +250,6 @@ if __name__ == '__main__':
     worker_updates_addr = ('localhost', 5001)
 
     rr_choice=[-1]
-
-    #dictLock=threading.Lock()
-    #queueLock=threading.Lock() 
-    #workLock=threading.Lock()
 
     incJob = threading.Thread(target=listen_incoming_jobs,args=((receive_jobs_addr),))
     incJob.start()
--- YACS/src/worker.py
+++ YACS/src/worker.py
@@ -1,120 +1,150 @@
 import socket
 import threading
 import time
-
 import sys
 import logging
 from datetime import datetime
 
-def add_to_pool(isEnd,job_id,task_id,duration):
-    poolLock.acquire()
-    #print("append acquired")
 
+def add_to_pool(isEnd,job_id,task_id,duration):
+    
+    poolLock.acquire()
+
+    #adding task details to the pool
     pool.append([isEnd,job_id,task_id,duration])
 
     poolLock.release()
-   # print("append released")
+
 
 def listen_to_master(messages_addr):
 
+    #defining the socket on which the worker and master communicate
     task_launch_message = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     task_launch_message.bind(messages_addr)
     task_launch_message.listen()
+    
     while True:
-        #print("Listening to master....")
+        #establish connection with the master
         conn, master_address = task_launch_message.accept()
+        
         while True:
+            #receiving data from the master
             data = conn.recv(2048)
+            
             if data:
+                
                 data = data.decode('utf-8')
                 isEnd,job_id,task_id,duration,schedule_algo=data.split(',')
                 
                 if logFlag[0]:
-                	fileName=schedule_algo+'/worker'+worker_id+'.csv'
-                	print("HERE: ",fileName) 
-                	logging.basicConfig(level=logging.INFO,filename=fileName, filemode='w', format='%(message)s')
-                	logging.info('Type,ID,Time')
-                	logFlag[0]=0
+                    #initialize logging files and parameters
+                    
+                    fileName='../data/'+schedule_algo+'/worker'+worker_id+'.csv' 
+                    logging.basicConfig(level=logging.INFO,filename=fileName, filemode='w', format='%(message)s')
+                    logging.info('Type,ID,Time')
+                    logFlag[0]=0
                 
                 now = datetime.now()
-                #currTime = now.strftime("%H:%M:%S")
+
+                #storing start time of the task
                 taskLog[task_id]=now
                 
+                #adding the task to the pool
                 add_to_pool(isEnd,job_id,task_id,int(duration))
+
             else:
-              # print("Master chan is silent")
+                #print("Master chan is silent")
                 break
+
         conn.close()
 
+
 def sendNotif(job_id,task_id):
+    
+    #sends updates to the master about the job status
+    #communication happens over port 5001
     print("TASK "+task_id+" DONE")
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as toMaster:
         toMaster.connect(("localhost", 5001))
         message=worker_id+','+job_id+','+task_id
-        toMaster.send(message.encode()) #send task
+        toMaster.send(message.encode()) 
 
 
 def working():
+
     while True:
 
+        #acquire the lock to prevent race conditions
         poolLock.acquire()
-       # print("working acquired")
 
         if pool:
             time.sleep(1)
 
+            #stores tasks which have have been completed and need to be removed the pool
             need_to_pop = list()
 
             for i in range(len(pool)):
-                print(pool,"\n")
+
                 pool[i][3]=pool[i][3]-1
+                
                 if pool[i][3]==0:
                 
                     currTime = datetime.now()
-                    #currTime = now.strftime("%H:%M:%S")
                 
+                    #calculating the time taken for the completion of the task
                     timeDiff=currTime-taskLog[pool[i][2]]
                     taskTime=timeDiff.total_seconds()
+
+                    #logging the task completion time
                     logging.info('TASK,'+pool[i][2]+','+str(taskTime))
+                    
                     if pool[i][0]=="1":
+
+                        #checking if the job has been completed and logging the job completion time
                         jobTime=currTime.strftime("%H:%M:%S")
                         logging.info('JOB,'+pool[i][1]+','+jobTime)
                 
-                
+                    #sending a message to the master that a particular task has completed
                     sendNotif(pool[i][1],pool[i][2])
-                    #print("POP: ",pool.pop(i),"\n")
+                    
                     need_to_pop.append(i)
         
             for i in sorted(need_to_pop,reverse=True):
                 pool.pop(i)
 
+        #release lock as updation of shared data structure has finished
         poolLock.release()
-       # print("working released")
-
-            
-
 
 
 if __name__ == '__main__':
 
+    #reading in the port number and id of the worker as system arguments
     port  = int(sys.argv[1])
     worker_id = sys.argv[2]
-    messages_addr = ('localhost', port)
     
+    #Flag variable to check if we need to 
     logFlag=[1]  #NEED TO MAYBE CHANGE IMPLEMENTATION OF THIS LATER
-    #fileName='worker'+worker_id+'.csv'
-    #logging.basicConfig(level=logging.INFO,filename=fileName, filemode='w', format='%(message)s')
+    
+    #keeps track of start time of task in order to calculate the task run time
     taskLog={}
-
-    poolLock=threading.Lock()
 
     pool=[]
 
+    '''
+    pool is a list which contains the tasks currently running in the pool.
+    Since this is a shared data structure we will need to add a lock when we update its contents
+    '''
+
+    poolLock=threading.Lock()
+
+    #defining the port number on which the wroker needs to communicate with the master
+    messages_addr = ('localhost', port)
+
+    #defining a thread which is used by the worker to listen to the master
     masterListen = threading.Thread(target=listen_to_master,args=((messages_addr),))
     masterListen.start()
 
+    # defining a thread which performs the working or execution of tasks on the worker
     doTask= threading.Thread(target=working)
     doTask.start()
-
-    
-    
